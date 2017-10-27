b8c252c3386cb035950d8d4e38624565d25f9934
Richard Honor <me@r-honor.uk>
2017-10-27 18:20:41 +0100

    Latest translation to appear at front of array

--- src/reducers/translations.js
+++ src/reducers/translations.js
@@ -3,7 +3,7 @@ import { TRANSLATE_INPUT } from '../actions/translate';
 export default function(state = [], action) {
   switch(action.type){
     case TRANSLATE_INPUT:
-      return [...state, action.payload];
+      return [action.payload, ...state];
     default:
       return state;
   }
